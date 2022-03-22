b8f7f7d8b84e6a89028ad297b2a8aed865a95084
Mario <mario.makdis@outlook.com>
2022-03-22 22:18:08 +0100

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,13 @@ export function Main({
 }) {
   return <><img src={logo} className="App-logo" alt="logo" />
             <h1 className="header">CurtainOS</h1>
-            <p className="text">a free and open source operating system, built from scratch.</p></>;
+            <p className="text">a free and open source operating system, built from scratch.</p>
+            <p className="rules">We follow three principles:</p>
+            <ol className="rules">
+              <li>An operating system should serve its user</li>
+              <li>The user knows whats best</li>
+              <li>freedom is achieved through choice</li>
+            </ol>
+            </>;
 }
   
